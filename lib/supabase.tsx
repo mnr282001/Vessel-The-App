ef492c8d17b4dfb6bf472c74e19160fe9b4b08f2
Nayab Rehmat <mnr282001@gmail.com>
2024-06-13 16:07:13 -0500

    center avatar; format files; add environment variables for supabase

--- lib/supabase.tsx
+++ lib/supabase.tsx
@@ -1,10 +1,10 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://yfuosxhlcbozqucpasfp.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmdW9zeGhsY2JvenF1Y3Bhc2ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgwODEwNTAsImV4cCI6MjAzMzY1NzA1MH0.xsG-T8ZD7GP6ejvSufwbKSCcCBPaHOrJ6eOh6703qlc"
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_PROJECT_URL
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_PUBLIC
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(supabaseUrl!, supabaseAnonKey!, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
