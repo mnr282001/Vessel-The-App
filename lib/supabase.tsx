051f78169f408582720aa1b9c4ca610a76b700c2
Nayab Rehmat <mnr282001@gmail.com>
2024-06-13 15:31:04 -0500

    update app.tsx to use auth components

--- lib/supabase.tsx
+++ lib/supabase.tsx
@@ -1,10 +1,10 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_PROJECT_URL
-const supabaseAnonKey = process.env.SUPABASE_ANON_PUBLIC
+const supabaseUrl = "https://yfuosxhlcbozqucpasfp.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmdW9zeGhsY2JvenF1Y3Bhc2ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgwODEwNTAsImV4cCI6MjAzMzY1NzA1MH0.xsG-T8ZD7GP6ejvSufwbKSCcCBPaHOrJ6eOh6703qlc"
 
-export const supabase = createClient(supabaseUrl!, supabaseAnonKey!, {
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
